3845996f3e28bacfbfa7d4bcf14ee02863247fd2
johnathantng <johnathan.tng@gmail.com>
2019-07-12 03:37:57 -0700

    added proper links to anchor tags

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,15 @@ import linLogo from '../static/icons/linkedin-logo.svg';
 function Footer() {
 	return (
 		<div className="footer-container">
-			<img src={behLogo} alt="behance-logo" />
-			<img src={emailIcon} alt="contact-email-icon" />
-			<img src={linLogo} alt="linkedin-logo" />
+			<a target="_blank" href="https://www.behance.net/kaicao">
+				<img src={behLogo} alt="behance-logo" />
+			</a>
+			<a href="mailto:test">
+				<img src={emailIcon} alt="contact-email-icon" />
+			</a>
+			<a target="_blank" href="https://www.linkedin.com/in/kaic/">
+				<img src={linLogo} alt="linkedin-logo" />
+			</a>
 		</div>
 	);
 }

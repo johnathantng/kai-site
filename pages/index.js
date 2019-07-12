37d74018a036c87dcbf800f91bd42673eb9de4f7
johnathantng <johnathan.tng@gmail.com>
2019-07-11 18:33:03 -0700

    fixed css spacing

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from 'react';
-
 import { withRouter } from 'next/router';
-/**
-how to use Router
-**
-{() => Router.push('/about')}
-*/
 
-// import Component from '../components/Component';
+import Slide from '../components/Slide';
 import NavBar from '../components/NavBar';
 import CardList from '../components/CardList';
 

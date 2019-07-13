ad9ab4a6053f30287bbd7dc6a16e9ce490caa996
johnathantng <johnathan.tng@gmail.com>
2019-07-12 23:51:37 -0700

    added boilerplate code for navbar-desktop

--- components/NavBarDesktop.js
+++ components/NavBarDesktop.js
@@ -1,6 +1,40 @@
-function NavBarDesktop() {
+import { useState, useEffect } from 'react';
+import Router from 'next/router';
+
+function NavBarDesktop(props) {
+	const { page } = props;
+	const [workSelected, setWorkSelected] = useState('');
+	const [aboutSelected, setAboutSelected] = useState('');
+	const [skillsSelected, setSkillsSelected] = useState('');
+	const [contactSelected, setContactSelected] = useState('');
+
+	useEffect(() => {
+		switch(page) {
+			case 'about':
+				return (setAboutSelected('selected'));
+			default:
+				return (setWorkSelected('selected'));
+		}
+	}, []);
+
 	return (
-		<div>Under construction</div>
+		<div className="nav-desktop-header">
+			<span onClick={() => Router.push('/index')}>Kai</span>
+			<div className="nav-desktop-container">
+				<div className={`nav-desktop-item ${workSelected}`}>
+					<span onClick={() => Router.push('/index')}>work</span>
+				</div>
+				<div className={`nav-desktop-item ${aboutSelected}`}>
+					<span onClick={() => Router.push('/about')}>about me</span>
+				</div>
+				<div className={`nav-desktop-item ${skillsSelected}`}>
+					<span onClick={() => Router.push('/skills')}>skills</span>
+				</div>
+				<div className={`nav-desktop-item ${contactSelected}`}>
+					<span onClick={() => Router.push('/contact')}>contact</span>
+				</div>
+			</div>
+		</div>
 	);
 }
 

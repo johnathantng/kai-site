ef65b2ed87c4892a87289642d3ffdee1dd7c192e
johnathantng <johnathan.tng@gmail.com>
2019-07-15 01:12:41 -0700

    added scrolling to about me - added skills to about me

--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,15 @@ import Footer from '../components/Footer';
 
 import '../sass/index.scss';
 
+import Photoshop from '../static/icons/photoshop.png';
+import Illustrator from '../static/icons/illustrator.png';
+import Xd from '../static/icons/xd.png';
+import Sketch from '../static/icons/sketch.svg';
+import InDesign from '../static/icons/indesign.png';
+import Lightroom from '../static/icons/lightroom.png';
+import Rhino from '../static/icons/rhino.png';
+import Solidworks from '../static/icons/sw.png';
+
 function About() {
   return (
   	<div>
@@ -25,7 +34,19 @@ function About() {
 	  			Throughout the course of my education,
 	  			I have developed an interdisciplinary skillset that allows me to
 	  			design solutions to problems in an effective and empathetic way.
+          <br /><br />
   			</p>
+        <span className="skills-heading">Skills</span>
+        <div className="skills-container">
+          <img className="skills-item" src={Photoshop} />
+          <img className="skills-item" src={Illustrator} />
+          <img className="skills-item" src={Xd} />
+          <img className="skills-item" src={Sketch} />
+          <img className="skills-item" src={InDesign} />
+          <img className="skills-item" src={Lightroom} />
+          <img className="skills-item" src={Rhino} />
+          <img className="skills-item" src={Solidworks} />
+        </div>
 			</div>
   	</div>
   );

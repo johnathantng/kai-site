043aa80a110fe4c751e38757eb8a97b668cf95cb
johnathantng <johnathan.tng@gmail.com>
2019-10-29 05:06:20 -0400

    sectioned off into components for new site

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { withRouter } from 'next/router';
 import Head from 'next/head';
 
-import NavBar from '../components/NavBar';
-import CardList from '../components/CardList';
-import Footer from '../components/Footer';
-
 import '../sass/index.scss';
 
 function App() {
@@ -22,9 +18,6 @@ function App() {
         />
   			<link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
   		</Head>
-  		<NavBar page="work"/>
-  		<Footer />
-  		<CardList />
   	</div>
   );
 }

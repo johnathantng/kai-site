0000d4a965ef9b3a06fd574e3929c078ff1dcce7
johnathantng <johnathan.tng@gmail.com>
2019-10-30 06:25:41 -0400

    added several new animations to landing page

--- components/Landing.js
+++ components/Landing.js
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from 'react';
 function Landing() {
     return (
         <div className="landing-container">
-            <span className="landing-text slide-right">
-                Welcome.
-            </span>
+            <div className="landing-text slide-right">
+                <span>Welcome</span>
+                <span className="red-dot">.</span>
+            </div>
         </div>
     )
 }

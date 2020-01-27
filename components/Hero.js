98d0260a76471d02bb046e322dd13277e671e918
johnathantng <johnathan.tng@gmail.com>
2020-01-27 01:36:10 -0500

    added 3 more components

--- components/Hero.js
+++ components/Hero.js
@@ -5,6 +5,11 @@ import Carousel from './Carousel';
 
 import sunImage from '../static/images/parallax-images/kai-website-sun.png';
 import slide1 from '../static/images/parallax-images/kai-slide-1.png';
+import slide2 from '../static/images/parallax-images/kai-slide-2.png';
+import slide3 from '../static/images/parallax-images/kai-slide-3.png';
+import slide4 from '../static/images/parallax-images/kai-slide-4.png';
+import slide5 from '../static/images/parallax-images/kai-slide-5.png';
+import slide6 from '../static/images/parallax-images/kai-slide-6.png';
 
 function Hero() {
     return (
@@ -26,6 +31,24 @@ function Hero() {
                     <img 
                         src={slide1}
                         className="slide-1"
+                    />
+                </ParallaxComponent>
+                <ParallaxComponent
+                    top={0}
+                    speed={0.8}
+                >
+                    <img 
+                        src={slide2}
+                        className="slide-2"
+                    />
+                </ParallaxComponent>
+                <ParallaxComponent
+                    top={0}
+                    speed={0.5}
+                >
+                    <img 
+                        src={slide3}
+                        className="slide-3"
                     />
                 </ParallaxComponent>
             </div>

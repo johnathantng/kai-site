70917a5214c98c5e9942f7fc0dc921a0eb278618
johnathantng <johnathan.tng@gmail.com>
2020-01-27 01:30:19 -0500

    rolling on general gist of parallax components

--- components/Hero.js
+++ components/Hero.js
@@ -1,19 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, StrictMode } from 'react';
 
 import ParallaxComponent from './ParallaxComponent';
 import Carousel from './Carousel';
 
+import sunImage from '../static/images/parallax-images/kai-website-sun.png';
+import slide1 from '../static/images/parallax-images/kai-slide-1.png';
+
 function Hero() {
     return (
-        <div className="hero-container">
-            <ParallaxComponent
-                top={0}
-                speed={0.6}
-            >
-                <div className="hero-text">
-                    <Carousel />
-                </div>
-            </ParallaxComponent>
+        <div>
+            <div className="hero-container">
+                <ParallaxComponent
+                    top={0}
+                    speed={0.6}
+                >
+                    <img 
+                        src={sunImage} 
+                        className="sun-image"
+                    />
+                </ParallaxComponent>
+                <ParallaxComponent
+                    top={0}
+                    speed={1.2}
+                >
+                    <img 
+                        src={slide1}
+                        className="slide-1"
+                    />
+                </ParallaxComponent>
+            </div>
         </div>
     );
 }
